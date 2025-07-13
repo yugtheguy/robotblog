4f252d3ae9544353445aaed6d0000ba9136134b0
Yug Deshmukh <yugdeshmukh58@gmail.com>
2025-07-13 18:49:33 +0530

    Add Render domain to ALLOWED_HOSTS

--- robot_blog/settings.py
+++ robot_blog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-40ok+04pi-sd#zgcc-nkw#-#iaw6=evq=1h5h_@9ne&9p!p88y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['robotblog.onrender.com']
 
 
 # Application definition
